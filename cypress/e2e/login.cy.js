b83ee4ca1ded621fbb315b84f7c05bb2ddd250bc
Luiz <luizitow@hotmail.com>
2026-04-03 15:43:39 -0400

    Adding fixtures and .gitignore was modified

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,8 +7,10 @@ describe('Login', () => {
 
   it('Login with valid credentials must permit access', () => {
     //Act
-    cy.get('#username').click().type('luiz.neto')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credentials').then(credentials => {
+      cy.get('#username').click().type(credentials.valid.username)
+      cy.get('#senha').click().type(credentials.valid.password)
+    })
     cy.screenshot('login-credentials')
     cy.contains('button', 'Entrar').click()
 
@@ -17,11 +19,13 @@ describe('Login', () => {
   })
   it('Login with invalid credentials must show error message', () => {
     //Act
-    cy.get('#username').click().type('luiz.neto')
-    cy.get('#senha').click().type('12312312')
+    cy.fixture('credentials').then(credentials => {
+      cy.get('#username').click().type(credentials.invalid.username)
+      cy.get('#senha').click().type(credentials.invalid.password)
+    })  
     cy.contains('button', 'Entrar').click()
 
     //Assert
     cy.get('.toast').should('be.visible').and('contain', 'Erro no login. Tente novamente.')
   })
-})
+})    
