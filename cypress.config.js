dc305e10f84b7436ba3a6327b04859b3698b0185
Luiz <luizitow@hotmail.com>
2026-04-03 20:15:54 -0400

    Adding HTML mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ module.exports = {
   allowCypressEnv: false,
 
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     baseUrl: 'http://localhost:4000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 };
